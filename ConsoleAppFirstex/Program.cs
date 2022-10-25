1daecf2235b67ebf779c87fd888d615214066b8d
Hp <Hp@DESKTOP-ID5FAVQ>
2022-10-25 11:51:03 +0200

    exercice1 et 2

--- ConsoleAppFirstex/Program.cs
+++ ConsoleAppFirstex/Program.cs
@@ -6,11 +6,52 @@ namespace ConsoleAppFirstex // Note: actual namespace depends on the project nam
     class Program
     {
 
+       
+
+
+
+        public static void InsererInt(int y)
+        {
+            
+            int[] tab = {1,2,3,4,5,6};
+
+            /*Console.WriteLine("veuillez entrer votre nombre");
+             int y =Console.Read();*/
+
+           
+            int x = tab.Length-1;
+
+            for(int i=x;i<=0;i--)
+            {
+                if (y > tab[i])
+                {
+                    tab[i] = tab[i-1];
+                    i = i - 1;
+                    tab[i] = y;
+                   
+                    
+                }
+
+            }
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.WriteLine(tab[i]);
+            }
+
+
+
+        }
+
+
+
+
+
         public static void Estpremier(int x) 
         {
             /*Console.WriteLine("veuillez entrer nombre");
             int Entre = Console.Read();*/
-
+            
+           
             int verif = 0;
             for (int i = 2; i <= Math.Sqrt(x); i++)
             {
@@ -39,7 +80,8 @@ namespace ConsoleAppFirstex // Note: actual namespace depends on the project nam
         
         {
 
-            Estpremier(5);
+           // Estpremier(5);
+            InsererInt(9);
            
         }
 }
