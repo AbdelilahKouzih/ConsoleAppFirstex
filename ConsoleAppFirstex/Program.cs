7b0d1dbc6f2aa7b166734a16e6f394c23d68b92b
Hp <Hp@DESKTOP-ID5FAVQ>
2022-10-25 10:35:53 +0200

    firstproject

--- ConsoleAppFirstex/Program.cs
+++ ConsoleAppFirstex/Program.cs
@@ -1,16 +1,89 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace ConsoleAppFirstex // Note: actual namespace depends on the project name.
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+
+        public static void Estpremier(int x) 
         {
-            Console.WriteLine();
-            Console.Read()
+            /*Console.WriteLine("veuillez entrer nombre");
+            int Entre = Console.Read();*/
 
+            int verif = 0;
+            for (int i = 2; i <= Math.Sqrt(x); i++)
+            {
+                if (x % i == 0)
+                {
+                    verif++;
+                }
 
+            }
 
+            if (verif != 0)
+            {
+                Console.WriteLine("votre nombre n'est pas premier");
+            }
+            else
+            {
+                Console.WriteLine("votre nombre est premier");
+
+            }
         }
-    }
+
+
+
+        static void Main(string[] args)
+        
+        
+        {
+
+            Estpremier(5);
+           
+        }
 }
+        
+        
+        
+        
+        
+
+        
+
+
+
+
+
+}
+
+
+
+
+    /* Console.WriteLine("veuillez entrer nombre");
+     int Entre = Console.Read();
+
+
+     int verif=0; 
+     for(int i=2;i<= Math.Sqrt(Entre);i++)
+     {
+         if(Entre%i==0)
+         {
+             verif++;
+         }
+
+     }
+
+     if (verif != 0)
+     { 
+         Console.WriteLine("votre nombre n'est pas premier");
+     }
+     else
+     { 
+         Console.WriteLine("votre nombre est premier");
+
+     }
+ }
+}
+}*/
+   
